99cf0ea41553ca363f0a3937351f0b7b9aa57c18
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 04:11:55 -0700

    initial

---
+++ ASP.NET.1607.Day1.Task4.Suhov/CheckSortingType.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET._1607.Day1.Task4.Suhov
+{
+    public static class CheckSortingType
+    {
+        public static string CheckType(int numb)
+        {
+            if (IsMono(numb))
+                return "Mono";
+            else
+            if (IsDecrease(numb))
+                return "Desreasing";
+            else
+            if (IsIncrease(numb))
+                return "Increasing";
+            else
+            if (IsIncreaseUnStr(numb))
+                return "IncreaseLax";
+            else
+            if (IsDecreaseUnStr(numb))
+                return "DesreaseLax";
+            else
+                return "NoSort";
+        }
+        private static bool IsMono(int numb)
+        {
+            string strnum = numb.ToString();
+            for (int i = 0; i < strnum.Length - 1; i++)
+            {
+                if (strnum[i] != strnum[i + 1])
+                    return false;
+            }
+            return true;
+        }
+        private static bool IsIncreaseUnStr(int numb)
+        {
+            string strnum = numb.ToString();
+            for (int i = 0; i < strnum.Length - 1; i++)
+            {
+                if (strnum[i] <= strnum[i + 1])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+        private static bool IsDecreaseUnStr(int numb)
+        {
+            string strnum = numb.ToString();
+            for (int i = 0; i < strnum.Length - 1; i++)
+            {
+                if (strnum[i] >= strnum[i + 1])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+        private static bool IsDecrease(int numb)
+        {
+            string strnum = numb.ToString();
+            for (int i = 0; i < strnum.Length - 1; i++)
+            {
+                if (strnum[i] < strnum[i + 1] || strnum[i] == strnum[i + 1]) return false;
+            }
+            return true;
+        }
+        private static bool IsIncrease(int numb)
+        {
+            string strnum = numb.ToString();
+            for (int i = 0; i < strnum.Length - 1; i++)
+            {
+                if (strnum[i] > strnum[i + 1] || strnum[i] == strnum[i + 1]) return false;
+            }
+            return true;
+        }
+    }
+}
