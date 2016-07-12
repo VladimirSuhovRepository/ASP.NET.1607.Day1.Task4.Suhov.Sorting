d32c3cf2627e647f02358a0c1ea321695675b6cc
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 04:54:07 -0700

    initial

--- ASP.NET.1607.Day1.Task4.Suhov/CheckSortingType.cs
+++ ASP.NET.1607.Day1.Task4.Suhov/CheckSortingType.cs
@@ -8,6 +8,9 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
 {
     public static class CheckSortingType
     {
+        /// <summary>Checks number digits sorting type</summary> 
+        /// <param name="value">Number which should be checked </param> 
+        /// <returns>The number <paramref name="n"/> has number which are sorted: <paramref name="value"/></returns> 
         public static string CheckType(int numb)
         {
             if (IsMono(numb))
@@ -27,6 +30,7 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
             else
                 return "NoSort";
         }
+        /// <summary>Checks whether number digits sorting type in Mono</summary> 
         private static bool IsMono(int numb)
         {
             string strnum = numb.ToString();
@@ -37,6 +41,7 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
             }
             return true;
         }
+        /// <summary>Checks whether number digits are increasing lax</summary> 
         private static bool IsIncreaseUnStr(int numb)
         {
             string strnum = numb.ToString();
@@ -49,6 +54,7 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
             }
             return true;
         }
+        /// <summary>Checks whether number digits are decreasing lax</summary>  
         private static bool IsDecreaseUnStr(int numb)
         {
             string strnum = numb.ToString();
@@ -61,6 +67,7 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
             }
             return true;
         }
+        /// <summary>Checks whether number digits are decreasing</summary> 
         private static bool IsDecrease(int numb)
         {
             string strnum = numb.ToString();
@@ -70,6 +77,7 @@ namespace ASP.NET._1607.Day1.Task4.Suhov
             }
             return true;
         }
+        /// <summary>Checks whether number digits are increasing</summary> 
         private static bool IsIncrease(int numb)
         {
             string strnum = numb.ToString();
