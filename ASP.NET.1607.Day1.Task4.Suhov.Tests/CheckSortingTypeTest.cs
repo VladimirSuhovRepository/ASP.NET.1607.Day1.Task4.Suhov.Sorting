fc4fec771c59b483df352e6affb749966932b951
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 04:49:36 -0700

    initial

---
+++ ASP.NET.1607.Day1.Task4.Suhov.Tests/CheckSortingTypeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ASP.NET._1607.Day1.Task4.Suhov.Tests
+{
+    [TestClass]
+    public class CheckSortingTypeTest
+    {
+        [TestMethod]
+        public void TestCheckSortingTypeArrays()
+        {
+            // Arrange
+            var arrays = new List<int>() { 123456, 654321, 1122334455, 99887766, 111111, 123654321  };
+            string[] result = new string[] { "Increasing", "Desreasing", "IncreaseLax", "DesreaseLax", "Mono", "NoSort" };
+            // Act
+            // Assert
+            int i = 0;
+            arrays.ForEach(delegate (int numb) {
+                Assert.AreEqual(CheckSortingType.CheckType(numb), result[i]);
+                i++;
+            });
+
+        }
+    }
+}
