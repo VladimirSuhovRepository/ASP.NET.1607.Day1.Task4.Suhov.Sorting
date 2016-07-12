4c0caa47552fe41f423615ff79295e505ac91616
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 04:09:16 -0700

    Add project files.

---
+++ ASP.NET.1607.Day1.Task4.Suhov/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET._1607.Day1.Task4.Suhov
+{
+    public class Class1
+    {
+    }
+}
